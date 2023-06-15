1be5227a8bcbec9250025d5ecb68cbd1640d9204
Zzerkow <leo.filsnoel@gmail.com>
2023-06-16 00:05:57 +0200

    responsive V1 deploy

--- src/Pages/Actions.js
+++ src/Pages/Actions.js
@@ -4,15 +4,18 @@ import Footer from '../Component/Footer.js'
 import './Actions.css'
 import '../font.css';
 import ActionPic1 from '../Img/Action_Pic1.png';
+import ActionPic2 from '../Img/Action_Pic2.png';
+import ActionPic3 from '../Img/Action_Pic3.png';
+import ActionPic4 from '../Img/Action_Pic4.png';
 
 export default function Actions(){
     {/* Constantes */}
 
     return(
         <>
-            {/* Première page */}
+            {/* Première partie */}
 
-            <div className='white-page-container'>
+            <div className='white-page-container-action'>
 
                 <div className='information-container'>
 
@@ -35,13 +38,109 @@ export default function Actions(){
                         <img className='picture' src={ActionPic1}/>
                         <p className='picture-description-black'>Description :</p>
 
-
                     </div>
 
                 </div>
 
             </div>
-    
+
+            {/* Deuxième partie */}
+
+            <div className='black-page-containe-left-anchor'>
+
+                <div className='anchor-left-picture-container'>
+
+                    <div className='right-picture-container'>
+
+                        <img className='picture' src={ActionPic2}/>
+                        <p className='picture-description-white'>Description :</p>
+
+                    </div>
+
+                </div>
+
+                <div className='information-container'>
+
+                    <p className='title-information-white'>Accompagnement des<span className='yellow-color'> professionnels</span>.</p>
+
+                    <p className='text-information-white'>E’Kollectif participe à la veille des éducateurs spécialisés et autres professionnels de la santé. 
+                    Une fois par mois un groupe de travail se réunit. La préparation se fait à partir d’un thème choisi par les participants du mois précédent. 
+                    La Présidente d’E’Kollectif fournit les éléments soutenant le sujet (vidéos,  support écrit…) aux responsables locaux qui préparent alors l’organisation de la rencontre. 
+                    </p>
+
+                    <MoreButtonWhite/>
+
+                </div>
+
+            </div>
+
+            {/* Troisième partie */}
+
+            <div className='white-page-container-right-anchor'>
+
+                <div className='information-container'>
+
+                    <p className='title-information-black'>La <span className='blue-color'>sensibilisation</span> est la <span className='blue-color'>clé</span> de la <span className='blue-color'>prévention</span>.</p>
+
+                    <p className='big-text-information-black'>Dans la société béninoise le fait d’avoir un enfant porteur de handicap est encore souvent considéré comme honteux, voire porteur de malédiction. 
+                    De ce fait les enfants, surtout ceux porteur d’un handicap invisible, sont cachés, exclus de la société. Parfois même, faute d’informations et de soutien,
+                    les familles vont chercher de l’aide dans les traditions vaudous, et cela peut être fatal à l’enfant. Aussi, afin de sensibiliser la population des journées d’information sont organisées
+                    </p>
+
+                    <p className='big-text-information-black'>La journée mondiale de sensibilisation à l’autisme est un moment fort de l’année qui permet de toucher un large public. 
+                    E’Kollectif participe a cette occasion à l’émission radio « Mon handicap et moi » diffusée sur ADO FM, organise une caravane à travers la ville, avec une halte dans un lieu stratégique permettant à la population interpelée de se renseigner à la fois sur l’autisme mais aussi sur les possibilités de prise en charge,
+                    et un article paraît dans la presse écrite.
+                    </p>               
+
+                </div>
+
+                <div className='anchor-right-picture-container'>
+
+                    <div className='center-picture-container'>
+
+                        <img className='picture' src={ActionPic3}/>
+                        <p className='picture-description-black'>Description :</p>                        
+
+                    </div>                   
+
+                </div>
+
+            </div>      
+
+            {/* Quatrième partie */}
+
+            <div className='black-page-containe-left-anchor'>
+
+                <div className='anchor-left-picture-container'>
+
+                    <div className='right-picture-container'>
+
+                        <img className='picture' src={ActionPic4}/>
+                        <p className='picture-description-white'>Description :</p>
+
+                    </div>
+
+                </div>
+
+                <div className='information-container'>
+                   
+                    <p className='text-information-white'>Toujours lors de son séjour Nadège, notre Présidente, organise une rencontre avec les familles d’enfants porteurs de handicap. En 2022 le thème en a été « Famille et handicap » afin d’évoquer le soutien à la parentalité et la scolarisation et l’accompagnement spécialisé. 
+                    En parallèle les responsables interviennent auprès de nos partenaires locaux lorsque ceux-ci organisent des manifestations.
+                    </p>
+
+                    <p className='text-information-white'>Ce fut le cas avec Nouvelles Chances Bénin ONG lors de la semaine  « 7 Social days » ou pour notre Présidente, 
+                    lors de son invitation pour intervenir en  visioconférence à l’occasion de la journée de l’autisme auprès de  l’Association Culturelle et Educative Star Plus – ACESP ONG.
+                    </p>
+
+
+
+                </div>
+
+            </div>
+
+            <Footer />
+
+
         </>
     );
 }
@@ -54,4 +153,13 @@ function MoreButton(){
       </button>
   
     );
-  }
+}
+
+function MoreButtonWhite(){
+    return(
+      <button className='more-button-white'>
+        <p className='more-button-text-white'>En savoir plus</p>
+      </button>
+  
+    );
+}
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,7 +5,7 @@ import picmain2 from '../Img/PicMain2.png';
 import picmain3 from '../Img/PicMain3.png';
 import picmain4 from '../Img/PicMain4.png';
 import actualityexemple from '../Img/actuality-exemple.png';
-import arrowmore from '../Img/arrowmore.png';
+import arrowmore from '../Img/arrowmore.svg';
 import logovidjovi from '../Img/logo-vidjovi.png';
 import logoncb from '../Img/logo-ncb.png';
 import logonISFES from '../Img/logo-ISFES.png';
@@ -17,340 +17,185 @@ import './Home.css'
 
 export default function Home(){
     {/* Constantes */}
-
-    const Picture1 = picmain1;
-    const Picture2 = picmain2;
+        
     const ActualityPicture = actualityexemple;
     const Picture3 = picmain3;
     const Picture4 = picmain4;
-    const Picturedesc = 'lorem ipsum dolerean big mac';
     const partner1 = { photo: logovidjovi, titre:'VIDJOVI "L\'enfant digne"', description :'Association française dont le siège est à Suze-la-Rousse, dont le but est de soutenir l\'éducation au Bénin et de prévenir l\'abandon et l\'exploitation des enfants.'};
     const partner2 = { photo: logoncb, titre:'NOUVELLES CHANCES BENIN ONG', description :'L\'ONG ayant notamment pour mission la prise en charge d\'apprenants handicapés intellectuels en milieux scolaires nous intervenons auprès d\'elle lors des manifestations qu\'elle organise.Ce fut ainsi le cas lors des 7 SOCIAL DAYS 2022.'};
     const partner3 = { photo: logonISFES, titre:'Institut Supérieur de Formation des Educateurs Spécialisés', description :'Par notre partenariat nous disposons d\'un local au sein de l\'établissement, ce qui nous permet d\'avoir un contact direct avec les futurs professionnels. Ces derniers ont accès au matériel dont dispose l\'association et peuvent participer aux divers groupes de travail et activités proposés par l\'association.'};
     const partner4 = { photo: logonchrysalide, titre:'ONG la CHRYSALIDE Bénin', description :'Cette organisation à but non lucratif est spécialisée dans l\'accompagnement des enfants et jeunes handicapés intellectuels depuis 1998. Nous soutenons ses actions et réciproquement'};
     const partnersline1 = {partner1 : partner1, partner2 : partner2};
     const partnersline2 = {partner3 : partner3, partner4 : partner4};
+
+    const nousconnaitres ="nous connaître";
   
     return(
-    <div>      
+    <>      
   
-        {/* 1ère Partie de la page plus petite que les autres ducoup 990px de hauteur */}
-  
-        {/* Phone Part */}
-        <div className='first-Content-Container'>
-  
-          <div className='bottom-content-text'>
-  
-            <p className='title-text'>Se <span className='pink-color'>battre</span> pour les <span className='pink-color'>autres</span>, plus qu'une <span className='pink-color'>vocation</span>.</p>
-            <p className='text'>Nous intervenons au Bénin car il est urgent de changer le regard sur le handicap,
-            et plus encore sur le handicap invisible. Avoir un enfant porteur de handicap au sein de sa famille est considéré comme honteux,
-              voire porteur de malédiction. E’Kollectif a pour mission de développer l’accompagnement de ces enfants, de participer à la veille des professionnels et de  sensibiliser les familles et la population.</p>
-            <MoreButton/>
-  
-  
-          </div>
-  
-          <div className='top-content-text'>
-         
-            <div className='picture-container'>
-              <PictureMainPage1 data={Picture1}/>
-              <PictureDescBlack data={Picturedesc}/>            
-            </div>
-          
-          </div>
-  
-        </div>
-  
+        {/* Première partie */}       
       
-        <div className='second-Content-Container'>
-  
-          <div className='second-bottom-container'>
-  
-            <PictureMainPage2 data={Picture2}/>
-  
-          </div>
-  
-          <div className='second-top-container'>
-            
-          <p className='title-text-2'>Qui sommes-<span className='yellow-color'>nous</span>?</p>
-          <p className='text-2'>E’Kollectif est une association loi 1901 à but non lucratif, d’intérêt général, créée en août 2020 pour  apporter une aide aux enfants porteurs de handicap en Afrique, et notamment au Bénin.</p>        
-          <p className='text-2-bis'>Si la gestion des activités se fait depuis la France E’Kollectif dispose au Bénin, à Cotonou, d’un local géré par deux responsables salariés de l’association.</p>
-            
+        <div className='white-page-container-right-anchor'>
+
+              <div className='information-container'>
+
+              <p className='title-information-black'>Se <span className='pink-color'> battre</span> pour les <span className='pink-color'>autres</span>, plus qu'une <span className='pink-color'>vocation</span>.</p>
+
+                  <p className='text-information-black'>Nous intervenons au Bénin car il est urgent de changer le regard sur le handicap, et plus encore sur le handicap invisible. 
+                  Avoir un enfant porteur de handicap au sein de sa famille est considéré comme honteux, voire porteur de malédiction. 
+                  E’Kollectif a pour mission de développer l’accompagnement de ces enfants, de participer à la veille des professionnels et de  sensibiliser les familles et la population.
+                  </p>
+
+                  <MoreButton data = {nousconnaitres}/>
+
+              </div>
+
+              <div className='anchor-right-picture-container'>
+
+                  <div className='center-picture-container'>
+
+                      <img className='picture' src={picmain1}/>
+                      <p className='picture-description-black'>Description :</p>                     
+
+                  </div>                   
+
+              </div>
+
           </div>      
-        </div>
-        
-  
-        <div className='third-content-container'>
-  
-          <div className='third-top-container'>
-  
-            <TitleActuality/>
-            <MainActuality data={ActualityPicture}/>
-  
-          </div>
-  
-          <div className='third-bottom-container'>
-  
-            <OldActuality data={ActualityPicture}/>
-            <div className='br'></div>
-            <OldActuality data={ActualityPicture}/>
-  
-          </div>
-  
-        </div>
-  
- 
-        <div className='fourth-content-container'>
 
-          <div className='fourth-bottom-container'>
-  
-            <Titlepresentation/>
-            <p className='text-2'>Nadège CHAPPE, Présidente de l’association, elle-même en situation de handicap, est éducatrice spécialisée au Centre Ressources Autisme Rhône-Alpes.</p> 
-            <p className='text-2-bis'>Lors d’un séjour de plusieurs mois, en tant que bénévole au sein d’une ONG de Cotonou, elle a été frappée par le manque de moyens mis à la disposition des familles d’enfants porteurs de handicap
-              et en parallèle, le manque d’outils éducatifs  mis à la disposition des professionnels.
-              De son retour en France elle a réfléchi à l’aide qu’elle pourrait leur apporter et a créé E’Kollectif.</p> 
-  
-          </div>
+        {/* Deuxième partie */}
 
-          <div className='fourth-top-container'>
-          
-          <div className='picture-container margin-fourth-picture'>
-            <PictureMainPage3 data={Picture3}/>
-            <PictureDescWhite data={Picturedesc}/>            
-          </div>
+        <div className='black-page-container'>
 
-        </div>
-  
-  
-          
-  
-        </div>
-   
-        <div className='fifth-content-container'>          
+          <div className='picture-container-left'>
 
-          <div className='fifth-bottom-container'>
-  
-            <div className='picture-container'>
-                <PictureMainPage4 data={Picture4}/>
-                <PictureDescBlack data={Picturedesc}/>            
+            <div className='center-picture-container'>
+
+                <img className='picture' src={picmain2}/>
+
             </div>
 
           </div>
 
-          <div className='fifth-top-container'>
-  
-            <TitleEquipe/>
-            <p className='text-5-france-titre'>En France :</p>
-            <p className='text-5-france'> E’Kollectif est dirigée par la Présidente fondatrice qui est entourée d’une équipe administrative, et elle s’appuie sur l’aide de bénévoles.</p>
-            <p className='text-5-benin-titre'>Au Bénin :</p>
-            <p className='text-5-benin'> A Cotonou E’Kollectif dispose d’un local géré par deux responsables salariés de l’association. Tous deux éducateurs spécialisés leurs connaissances du terrain permettent d’intervenir concrètement dans la prise en charge des difficultés rencontrées tant par les familles que par les professionnels.</p>
-            
-          </div>
-  
-        </div>
-  
-      
-        <div className='sixth-content-container'>
+          <div className='information-container'>
 
-          <p className='title-text-6'>Nos <span className='green-color'>Partenaires</span>.</p>
-  
-          <div className='partner-container'>
-            
-          <Partner2cards1line data={partnersline1}/>
-          <Partner2cards1linebis data={partnersline2}/>
-          </div>            
-  
+            <p className='title-information-white'>Qui sommes-<span className='yellow-color'> nous</span>?</p>
+
+            <p className='text-information-white'>E’Kollectif est une association loi 1901 à but non lucratif, d’intérêt général, créée en août 2020 pour apporter une aide aux enfants porteurs de handicap en Afrique, et notamment au Bénin.</p>
+
+            <p className='text-information-white'>Si la gestion des activités se fait depuis la France E’Kollectif dispose au Bénin, à Cotonou, d’un local géré par deux responsables salariés de l’association.</p>
+
+          </div>        
+        
         </div>
-      
-      <Footer/>   
-     
-    </div>         
+
+         {/* Troisième partie */}  
+
+        <div className='white-page-container'>
+
+          <div className='actuality-main-container'>
+
+            <div className='principal-actuality-card'>
+
+              <p className='title-information-black'>Les <span className='green-color'>Actualités</span> E'kollectif.</p>
+
+              <img className='main-actuality-picture' src={actualityexemple}/>      
+
+              <div className='date-more-container'>
+
+                <p className='date-actuality'>Le 25 Mars 2023</p>
+
+                <img src={arrowmore} className='more-actuality'/>                
+                
+              </div>     
+
+              <p className='title-main-actuality'>Titre Actualité</p>
+
+              <p className='text-main-actuality'>lorem ipsum grosse inondation dans le sud de la france et du bénin causé par thor en personne lorem ipsum grosse 
+                inondation dans le sud de la france et du bénin causé par thor en personne du bénin causé par thor en personne</p>  
+              
+            </div>
+
+          </div>
+
+          <div className='actuality-main-container'>
+
+            <div className='old-actuality-card'>
+
+              <img src={actualityexemple} className='old-actuality-picture' />
+
+              <div className='old-actuality-information'>
+
+                <div className='old-date-more-container'>
+
+                  <p className='old-date-actuality'>Le 25 Mars 2023</p>
+
+                  <img src={arrowmore} className='old-more-actuality'/> 
+
+                </div>
+
+                <p className='old-title-main-actuality'>Titre Actualité</p>         
+
+                 <p className='old-text-main-actuality'>lorem ipsum grosse inondation dans le sud de la france et du bénin causé par thor en personne lorem ipsum grosse 
+                inondation dans le sud de la france et du bénin causé par thor en personne du bénin causé par thor en personne</p>      
+                
+              </div>  
+
+            </div> 
+
+
+            <div className='old-actuality-card'>
+
+              <img src={actualityexemple} className='old-actuality-picture' />
+
+              <div className='old-actuality-information'>
+
+                <div className='old-date-more-container'>
+
+                  <p className='old-date-actuality'>Le 25 Mars 2023</p>
+
+                  <img src={arrowmore} className='old-more-actuality'/> 
+
+                </div>
+
+                <p className='old-title-main-actuality'>Titre Actualité</p>         
+
+                 <p className='old-text-main-actuality'>lorem ipsum grosse inondation dans le sud de la france et du bénin causé par thor en personne lorem ipsum grosse 
+                inondation dans le sud de la france et du bénin causé par thor en personne du bénin causé par thor en personne</p>      
+                
+              </div>  
+
+            </div>              
+
+          </div>
+
+
+          
+
+        </div>
+       
+    </>         
 
     );
 } 
 
+function MoreButton(props){
 
-function MoreButton(){
-    return(
-      <button className='more-button'>
-        <p className='more-button-text'>Nous connaître</p>
-      </button>
-  
-    );
-  }
-  
-  function PictureMainPage1(props) { 
-    const data = props.data
-  
-    return(
-     
-      <img className='picture-main-1' src={data}/>
-     
-    );
-  }
-  
-  function PictureMainPage2(props) {
-    const data = props.data
-    return(
-  
-      <img className='picture-main-2' src={data} />
-  
-    );
-  }
-  
-  function TitleActuality(){ 
-    return(
-  
-      <p className='title-text-3'>Les <span className='green-color'>Actualités</span> E'Kollectif.</p>
-  
-    );
-  }
-  
-  function MainActuality(props){
-    const data = props.data
-    return(
-  
-      <div className='main-card-actuality'>
-  
-        <img src={data} className='picture-main-actuality'/>
-        <div className='date-and-button-line'>
-          <span className='main-date-actuality'>Le 25 Mars 2023</span>
-          <img src={arrowmore} className='button-more-actuality'/>
-        </div>
-        <span className='main-title-actuatity'>Titre Actualité</span>
-        <p className='main-desc-actuality'>lorem ipsum grosse inondation dans le sud de la france et du bénin causé par thor en personne lorem ipsum grosse inondation
-         dans le sud de la france et du bénin causé par thor en personne du bénin causé par thor en personne</p>
-  
-      </div>
-    );
-  }
-  
-  function OldActuality(props){
-    const data = props.data
-    return(
-        
-      <div className='old-card-actuality'>
-  
-        <img src={data} className='picture-old-actuality'/>
-  
-        <div className='info-old-actuality-container'>
-  
-          <div className='old-date-button-line'>
-            <span className='old-date-actuality'>Le 25 Mars 2023</span>
-            <img src={arrowmore} className='button-more-actuality'/>
-          </div>
-  
-          <span className='old-title-actuality'>Titre Actualité</span>
-          <p className='old-desc-actuality'>lorem ipsum grosse inondation dans le sud de la france et 
-          du bénin causé par thor en personne lorem ipsum grosse inondation dans le...</p>
-  
-        </div>
-  
-      </div>
-    );
-  }
-  
-  function Titlepresentation(){
-    return(
-  
-      <p className='title-text-4'>Ceux qui ne se <span className='blue-color'>souviennent</span> pas du <span className='blue-color'>passé</span> sont <span className='blue-color'>condamnés</span> à le <span className='blue-color'>répéter</span>.</p>
-  
-    );
-  }
-  
-  function PictureMainPage3(props) { 
-    const data = props.data
-  
-    return(
-     
-      <img className='picture-main-3' src={data}/>
-  
-    );
-  }
-  
-  function PictureDescWhite(props){
-    const data = props.data
-    return(
-  
-      <p className='picture-desc-white'>Description : {data}</p>
-  
-    );
-  }
-  
-  function PictureDescBlack(props){
-    const data = props.data
-    return(
-  
-      <p className='picture-desc-black'>Description : {data}</p>
-  
-    );
-  }
-  
-  function PictureMainPage4(props) { 
-    const data = props.data
-  
-    return(
-     
-      <img className='picture-main-4' src={data}/>
-  
-    );
-  }
-  
-  function TitleEquipe(){
-    return(
-      
-      <p className='title-text-5'>Notre <span className='orange-color'>Equipe</span>.</p>
-  
-    );
-  }
-  
-  function Partner2byLine(props){
-    const { photo, titre, description } = props.data;
-    return(
-  
-      
-  
-      <div className='partner-card'>
-  
-        <div className='partner-logo-title'>
-  
-          <img src={photo}  className='partner-logo'/>
-          <p className='partner-title'>{titre}</p>
-  
-        </div>
-  
-        <p className='partner-desc'>{description}</p>
-  
-      </div>    
-  
-    );
-  }
-  
-  
-  function Partner2cards1line(props){
-    const {partner1, partner2} = props.data;
-    return(
-  
-      <div className='partner-line'>
-        
-        <Partner2byLine data={partner1}/>
-        <Partner2byLine data={partner2}/>
-  
-      </div>
-  
-    );
-  }
-  
-  function Partner2cards1linebis(props){
-    const {partner3, partner4} = props.data;
-    return(
-  
-      <div className='partner-line'>
-        
-        <Partner2byLine data={partner3}/>
-        <Partner2byLine data={partner4}/>
-  
-      </div>
-  
-    );
-  }
+  const data = props.data
+
+  return(
+    <button className='more-button'>
+      <p className='more-button-text'>{data}</p>
+    </button>
+
+  );
+}
+
+function MoreButtonWhite(){
+  return(
+    <button className='more-button-white'>
+      <p className='more-button-text-white'>En savoir plus</p>
+    </button>
+
+  );
+}
